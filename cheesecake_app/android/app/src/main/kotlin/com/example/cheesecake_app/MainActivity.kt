af6011389b7e3715fd17d1984ad13dab685e0181
Jatin-Khanijoan <jatinkhanijoan@gmail.com>
2025-01-10 23:35:12 +0530

    revamped project

--- cheesecake_app/android/app/src/main/kotlin/com/example/cheesecake_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cheesecake_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- extension/src/blocked.ts
+++
@@ -1,9 +0,0 @@
-// src/blocked.ts
-document.addEventListener("DOMContentLoaded", () => {
-  const resetButton = document.getElementById("resetTimer");
-
-  resetButton?.addEventListener("click", async () => {
-    await chrome.runtime.sendMessage({ action: "resetTimer" });
-    window.location.href = "https://youtube.com";
-  });
-});
